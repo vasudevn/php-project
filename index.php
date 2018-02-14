c6b6953c29176c64dab9e2958a32f94a3daa5bca
test1 <test1@mylabs.com>
2018-02-14 00:16:34 -0500

    keys removed

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-echo "This is git jenkins integration to deploy app on stage box successfully"; ?>
+echo "This is git jenkins integration to deploy app on stage box failed due to keys removed"; ?>
 
