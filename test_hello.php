ea76c6fe07e0cd82d23ce62c345e447d020f69d9
test1 <test1@mylabs.com>
2018-02-14 23:43:03 -0500

    code updated ver2

--- test_hello.php
+++ test_hello.php
@@ -3,7 +3,7 @@ ob_start();
 require("index.php");
 $result = ob_get_clean();
 
-if( $result != 'Hello World Guys GM !!!' ) {
+if( $result != 'Hello World Guys Good Morning !!!' ) {
    exit(1);
 }
 exit(0);
